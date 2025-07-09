dd2ea1151115cab4e48a6cee72abc1a9ad0fc4c6
Priya Lakhani <priyalakhani91@gmail.com>
2025-07-09 15:49:54 +0300

    create arrow functions

--- main.js
+++ main.js
@@ -1,61 +1,56 @@
-let page=1;
+let page = 1;
 let limit = 10;
-let max_reached= false;
+let max_reached = false;
 const url = `https://reqres.in/api/users?per_page=${limit}`;
 const reqHeaders = { "x-api-key": "reqres-free-v1" };
 
 const tableBody = document.getElementById("user-details");
-const controlBtn=document.querySelector(".controls");
+const controlBtn = document.querySelector(".controls");
 const loader = document.querySelector(".loader");
 
+const displayLoader = () => {
+  console.log("Loader", loader.style);
+  loader.style.display = "inline-block";
+};
 
-const displayLoader= ()=>{
-    console.log("Loader",loader.style);
-    loader.style.display="inline-block";
-}
+const hideLoader = () => {
+  loader.style.display = "none";
+};
 
-const hideLoader= ()=>{
-    loader.style.display="none";
-}
-
-const fetchUserData = (url,page=1) => {
-    displayLoader();
-    paginatedUrl = `${url}&&page=${page}`;
-    console.log("Fetching from url",paginatedUrl);
-    fetch(paginatedUrl, { headers: reqHeaders })
-  .then((res) => {
-    hideLoader();
+const fetchUserData = async (url, page = 1) => {
+  displayLoader();
+  const paginatedUrl = `${url}&page=${page}`;
+  console.log("Fetching from url", paginatedUrl);
+  try {
+    const res = await fetch(paginatedUrl, { headers: reqHeaders });
     if (res.ok) {
-      return res.json();
+        console.log("Response received");
+        const resJson = await res.json();
+        hideLoader();
+      populateData(resJson.data);
+    } else {
+      throw new Error("Network response was not ok");
     }
-    throw new Error("Network response was not ok");
-  })
-  .then((resJson) => {
-    console.log("Response received.");
-    populateData(resJson.data);
-  })
-  .catch((error) => {
+  } catch (error) {
+    hideLoader();
     console.error("There has been a problem with your fetch operation:", error);
-  });
+    tableBody.innerHTML = '<tr><td colspan="3">Error loading data. Please try again.</td></tr>';
+  }
+};
 
+const removeAllChildren=(parent)=> {
+   parent.innerHTML = '';
 }
 
-function removeAllChildren(parent) {
-        while (parent.firstChild) {
-            parent.removeChild(parent.firstChild);
-        }
-    }
-
-function populateData(resData) {
+const populateData=(resData)=> {
   if (!tableBody) {
     console.error("Table body element not found.");
     return;
   }
-  if (resData.length<limit){
-     max_reached=true;
-  }
-  else{
-    max_reached=false;
+  if (resData.length < limit) {
+    max_reached = true;
+  } else {
+    max_reached = false;
   }
   removeAllChildren(tableBody);
 
@@ -63,18 +58,18 @@ function populateData(resData) {
     const dataRow = document.createElement("tr");
 
     const dataValueFirstName = document.createElement("td");
-    dataValueFirstName.textContent  = first_name;
+    dataValueFirstName.textContent = first_name;
 
     const dataValueLastName = document.createElement("td");
-    dataValueLastName.textContent  = last_name;
+    dataValueLastName.textContent = last_name;
 
     const dataValueAvatar = document.createElement("td");
-    avatarImage = document.createElement("img");
+    const avatarImage = document.createElement("img");
     avatarImage.setAttribute("src", avatar);
     avatarImage.alt = "image";
 
     dataValueAvatar.appendChild(avatarImage);
-    
+
     dataRow.appendChild(dataValueAvatar);
     dataRow.appendChild(dataValueFirstName);
     dataRow.appendChild(dataValueLastName);
@@ -83,20 +78,15 @@ function populateData(resData) {
   }
 }
 
+controlBtn.addEventListener("click", (event) => {
+  console.log(event);
+  if (event.target.id == "next" && !max_reached) {
+    page++;
+    fetchUserData(url, page);
+  } else if (event.target.id == "previous" && page > 1) {
+    page--;
+    fetchUserData(url, page);
+  }
+});
 
-
-controlBtn.addEventListener("click",(event)=>{
-    console.log(event)
-    if (event.target.attributes.id.value=="next" && !max_reached){
-        page++;
-        fetchUserData(url,page);
-    }
-    else if (event.target.attributes.id.value=="previous" && page>1){
-        page--;
-        fetchUserData(url,page);
-    }
-    
-}
-)
-
-fetchUserData(url)
+fetchUserData(url);
