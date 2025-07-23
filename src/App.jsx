325edec69aeb98474c835186c8627640a74ab81e
Priya Lakhani <priyalakhani91@gmail.com>
2025-07-23 11:26:55 +0300

    setup routing with createbrowser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,36 @@
 import "./App.css";
 import Home from "./components/Home/Home";
-import Header from "./components/Header/Header";
+import Root from "./components/Root/Root";
 import About from "./components/About/About";
 import Products from "./components/Products/Products";
 import Product from "./components/Product/Product";
 import Profile from "./components/Profile/Profile";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+} from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="products" element={<Products />} />
+        <Route path="products/:product" element={<Product />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Route>
+    )
+  );
   return (
     <main>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="products" element={<Products />}>
-            <Route path=":product" element={<Product />} />
-          </Route>
-          <Route path="profile" element={<Profile />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </main>
   );
 }
