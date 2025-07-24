0e5db58959e59d3d40d7e75a1b9486414f920011
Priya Lakhani <priyalakhani91@gmail.com>
2025-07-24 17:49:22 +0300

    register styling with material ui

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,58 +1,114 @@
 import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router";
+import { useNavigate, Link } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
+import { Box, Button, Container, TextField, Typography } from "@mui/material";
 
 export default function Register() {
   const { login } = useAuth();
-  const navigateHome = useNavigate();
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
-    // watch,
     formState: { errors },
   } = useForm();
 
   return (
-    <>
-      <h1>Register</h1>
-      <form
-        onSubmit={handleSubmit((data) => {
-          console.log(data);
-          navigateHome("/");
-          login();
-        })}
+    <Container maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
       >
-        <label for="firstName">First Name:</label>
-        <input
-          id="firstName"
-          type="text"
-          {...register("firstName", { required: true })}
-        />
-        <label for="lastName">Last Name:</label>{" "}
-        <input
-          id="lastName"
-          type="text"
-          {...register("firstName", { required: true })}
-        />
-        <label for="email">Email:</label>{" "}
-        <input
-          id="email"
-          type="email"
-          {...register("email", { required: "Please fill in the email" })}
-        />
-        <p>{errors.email?.message}</p>
-        <label for="password">Password:</label>{" "}
-        <input
-          id="password"
-          type="password"
-          {...register("password", {
-            required: "Please fill in the password",
-            minLength: { value: 4, message: "Minimum length is 4" },
+        <Typography component="h1" variant="h4" color="primary" gutterBottom>
+          Register
+        </Typography>
+        <Box
+          component="form"
+          onSubmit={handleSubmit((data) => {
+            console.log(data);
+            navigate("/");
+            login();
           })}
-        />
-        <p>{errors.password?.message}</p>
-        <input type="submit" />
-      </form>
-    </>
+          sx={{ mt: 1 }}
+        >
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="firstName"
+            label="First Name"
+            autoComplete="given-name"
+            autoFocus
+            error={!!errors.firstName}
+            helperText={errors.firstName?.message}
+            {...register("firstName", {
+              required: "Please fill in the first name",
+            })}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="lastName"
+            label="Last Name"
+            autoComplete="family-name"
+            error={!!errors.lastName}
+            helperText={errors.lastName?.message}
+            {...register("lastName", {
+              required: "Please fill in the last name",
+            })}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            type="email"
+            autoComplete="email"
+            error={!!errors.email}
+            helperText={errors.email?.message}
+            {...register("email", { required: "Please fill in the email" })}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="password"
+            label="Password"
+            type="password"
+            autoComplete="new-password"
+            error={!!errors.password}
+            helperText={errors.password?.message}
+            {...register("password", {
+              required: "Please fill in the password",
+              minLength: { value: 4, message: "Minimum length is 4" },
+            })}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="info"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Register
+          </Button>
+          <Button
+            component={Link}
+            to="/login"
+            fullWidth
+            variant="outlined"
+            color="secondary"
+            sx={{ mt: 1 }}
+          >
+            Already have an account? Sign In
+          </Button>
+        </Box>
+      </Box>
+    </Container>
   );
 }
